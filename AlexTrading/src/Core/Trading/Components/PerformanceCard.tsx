d94d217f7230094bf66872f7851085089b3f70f4
Tathagata Guha Ray <nguharay71@gmail.com>
2026-01-10 00:57:05 +0530

    Add unrealized PnL

--- AlexTrading/src/Core/Trading/Components/PerformanceCard.tsx
+++ AlexTrading/src/Core/Trading/Components/PerformanceCard.tsx
@@ -4,12 +4,24 @@ import { Table, TableBody, TableCell, TableRow } from "@/Components/UI/table";
 import { Percent, Activity } from "lucide-react";
 
 
-export default function PerformanceCard({ statistics }) {
+interface PerformanceProperties {
+    statistics: {
+        pnl:    number;
+        numTrades: number;
+        winRate:    number;
+        sharpeRatio: number;
+    };
+    unrealizedPnl: number;
+}
+
+
+
+export default function PerformanceCard({ statistics, unrealizedPnl }) {
     return (
-        <Card className="bg-zinc-900 border-x border-t border-b border-zinc-800 flex-1 shadow-xl rounded-none">
+        <Card className="bg-zinc-900 border-x border-t border-b border-zinc-800 flex-1 shadow-xl rounded-none mt-1 h-fit">
             
             <CardHeader className="pb-2">
-                <CardTitle className="text-lg font-large flex items-center gap-4 text-white">
+                <CardTitle className="text-xl font-large flex items-center gap-4 text-white">
                     <Activity className="w-7 h-7 text-orange-400" />
                     Performance Metrics
                 </CardTitle>
@@ -19,38 +31,45 @@ export default function PerformanceCard({ statistics }) {
                 <Table>
                     <TableBody>
                         <TableRow className="border-zinc-800 hover:bg-zinc-800/50">
-                            <TableCell className="text-zinc-400 py-3">
+                            <TableCell className="text-zinc-200 py-3">Unrealized PnL</TableCell>
+                            <TableCell className={`text-right font-mono ${ unrealizedPnl >= 0 ? "text-green-400" : "text-red-400"}` }>
+                                { unrealizedPnl >= 0 ? '+' : '' }{ unrealizedPnl.toFixed(2) }
+                            </TableCell>
+                        </TableRow>
+
+                        <TableRow className="border-zinc-800 hover:bg-zinc-800/50">
+                            <TableCell className="text-zinc-200 py-3">
                                 Total PnL
                             </TableCell>
-                            <TableCell className={`text-right font-mono ${statistics.pnl >= 0 ? "text-green-500" : "text-red-500"}`}>
-                                {statistics.pnl >= 0 ? '+' : ''}{statistics.pnl.toFixed(2)}
+                            <TableCell className={`text-right font-mono ${ statistics.pnl >= 0 ? "text-green-500" : "text-red-500"}` }>
+                                { statistics.pnl >= 0 ? '+' : '' }${ statistics.pnl.toFixed(2) }
                             </TableCell>
                         </TableRow>
                         
                         <TableRow className="border-zinc-800 hover:bg-zinc-800/50">
-                            <TableCell className="text-zinc-400 py-3">
+                            <TableCell className="text-zinc-200 py-3">
                                 Trades Executed
                             </TableCell>
                             <TableCell className="text-right font-mono text-white">
-                                {statistics.numTrades}
+                                { statistics.numTrades }
                             </TableCell>
                         </TableRow>
                         
                         <TableRow className="border-zinc-800 hover:bg-zinc-800/50">
-                            <TableCell className="text-zinc-400 py-3 flex items-center gap-1">
+                            <TableCell className="text-zinc-200 py-3 flex items-center gap-1">
                                 Win Rate <Percent className="w-3 h-3" />
                             </TableCell>
                             <TableCell className="text-right font-mono text-white">
-                                {statistics.winRate.toFixed(2)}%
+                                {  statistics.winRate == 0 ? "Not enough data" : statistics.winRate.toFixed(2) + '%' }
                             </TableCell>
                         </TableRow>
                         
                         <TableRow className="border-zinc-800 hover:bg-zinc-800/50">
-                            <TableCell className="text-zinc-400 py-3">
+                            <TableCell className="text-zinc-200 py-3">
                                 Sharpe Ratio
                             </TableCell>
                             <TableCell className="text-right font-mono text-cyan-300">
-                                {statistics.sharpeRatio.toFixed(2)}
+                                { statistics.sharpeRatio.toFixed(2) == 0.00 ? "Not enough data" : 0.00 }
                             </TableCell>
                         </TableRow>
                     </TableBody>
